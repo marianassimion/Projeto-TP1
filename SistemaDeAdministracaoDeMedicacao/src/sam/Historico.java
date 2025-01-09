ceb8aaedad33ec2ffbd22eb406430f649388a38e
Dionilton <dionilton.os@gmail.com>
2025-01-08 21:12:28 -0300

    agora foi real oficial amém

---
+++ SistemaDeAdministracaoDeMedicacao/src/sam/Historico.java
@@ -0,0 +1,28 @@
+package sam;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Historico {
+
+    private ArrayList<RegistroDeAplicacao> historico;
+
+    public ArrayList<RegistroDeAplicacao> buscarPorPaciente (String cpfPaciente) {
+        //todo: implementar lógica do método
+        return this.historico;
+    }
+
+    public ArrayList<RegistroDeAplicacao> buscarPorDia (LocalDate data) {
+        //todo: implementar lógica do método
+        return this.historico;
+    }
+
+    public void listarHistorico() {
+        //todo: implementar lógica do método
+        System.out.println(this.historico);
+    }
+
+    public void cadastrarNovoRegistro (RegistroDeAplicacao registroDeAplicacao) {
+        this.historico.add(registroDeAplicacao);
+    }
+}
---
+++ SistemaDeAdministracaoDeMedicacao/src/sam/Prescricao.java
@@ -0,0 +1,11 @@
+package sam;
+
+import java.time.LocalDate;
+
+public class Prescricao {
+    private LocalDate dataDeValidade;
+
+    public LocalDate getDataDeValidade() {
+        return dataDeValidade;
+    }
+}
