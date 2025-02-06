9176c774acc70aefeded1258b1027aa8ffb66708
rute-fernds <rute.a.fernds@gmail.com>
2025-02-05 21:09:06 -0300

    de novo

--- Projeto-TP1-atualizao-telas-inicial-cadastro-edicao-busca/SistemaDeAdministracaoDeMedicacao/src/sam/Administrador.java
+++
@@ -1,9 +0,0 @@
-package sam;
-
-public class Administrador extends Pessoa{
-
-    public Administrador(String nome, String cpf, String dataNascimento) {
-        super(nome, cpf, dataNascimento);
-    }
-
-}
